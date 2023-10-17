b645f96fef2404607d05e8d66ed237b97efda686
Serzhan Kenesbek <serzhan.kenesbek1@gmail.com>
2023-10-17 02:46:35 +0200

    Update files and add feautures

--- assignment_4/public_html/js/script.js
+++ assignment_4/public_html/js/script.js
@@ -6,6 +6,9 @@ const signUpLink = document.getElementById("signup-btn");
 const signInForm = document.getElementById("signin");
 const signUpForm = document.getElementById("signup");
 
+// Define the interval for automatic sliding
+const interval = 2000; // 5000 milliseconds (5 seconds)
+
 // Function to toggle forms
 function toggleForms(formToShow, formToHide) {
   formToShow.style.display = "block";
@@ -14,7 +17,7 @@ function toggleForms(formToShow, formToHide) {
 
 // Function to set link colors
 function setLinkColor(activeLink, inactiveLink) {
-  activeLink.style.color = "orange";
+  activeLink.style.color = "#00ccff";
   inactiveLink.style.color = "#c0c0c0";
 }
 
@@ -28,18 +31,49 @@ function closePopup(event) {
 // Event listeners
 overlay.addEventListener("click", closePopup);
 
-signInLink.addEventListener("click", function (event) {
+signInLink.addEventListener("click", () => {
   toggleForms(signInForm, signUpForm);
   setLinkColor(signInLink, signUpLink);
 });
 
-signInMainLink.addEventListener("click", function (event) {
+signInMainLink.addEventListener("click", () => {
   overlay.style.display = "block";
   toggleForms(signInForm, signUpForm);
   setLinkColor(signInLink, signUpLink);
 });
 
-signUpLink.addEventListener("click", function (event) {
+signUpLink.addEventListener("click", () => {
   toggleForms(signUpForm, signInForm);
   setLinkColor(signUpLink, signInLink);
 });
+
+// Carousel to show 8 cryptocurrencies
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+
+let current = 0;
+
+// Function to go to the next slide
+function gotoNext() {
+  current = (current + 1) % slides.length;
+  updateSlideClasses();
+}
+
+// Function to update slide classes
+function updateSlideClasses() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active", "prev", "next");
+  }
+  const prev = (current - 1 + slides.length) % slides.length;
+  const next = (current + 1) % slides.length;
+  slides[current].classList.add("active");
+  slides[prev].classList.add("prev");
+  slides[next].classList.add("next");
+  slides[current].addEventListener("click", () => {
+    // Replace "desired-page.html" with the desired URL
+    window.location.href = "table.html";
+  });
+}
+
+// Start the automatic sliding
+const autoSlideInterval = setInterval(gotoNext, interval);
